7ff1c500052710d10e7c4e16f932e8ec6011fb09
adelquis-molinari <adelkis003@gmail.com>
2022-08-26 10:30:42 -0300

    user authentication

--- backend/controllers/user-controllers.js
+++ backend/controllers/user-controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/users.js"
 import generateId from "../helpers/generateId.js"
+import generateJWT from "../helpers/generateJWT.js"
 
 const register = async (req, res) =>{
 
@@ -7,7 +8,7 @@ const register = async (req, res) =>{
   const userEmail = await User.findOne({email})
   if (userEmail){
     const error = new Error("already registered user")
-    return res.status(404).json({ message : error.message })
+    return res.status(403).json({ message : error.message })
   }
 
   try {
@@ -20,7 +21,32 @@ const register = async (req, res) =>{
   }
 }
 
+const userAuthentication = async (req,res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({email})
+  if (!user) {
+    const error = new Error('No user exists.')
+    res.status(403).json({ message: error.message })
+  }
+  if (!user.confirm) {
+    const error = new Error('Unconfirmed user.')
+    return res.status(403).json({ message: error.message })
+  }
+  if(await user.checkPassword(password)){
+    const { _id, name, email } = user
+    res.json({
+      _id,
+      name,
+      email,
+      token: generateJWT(_id)
+    })
+  }else{
+    const error = new Error('Incorrect password.')
+    res.status(403).json({ message: error.message })
+  }
+}
 
 export {
-    register
+    register,
+    userAuthentication
 }
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { register } from "../controllers/user-controllers.js"
+import { register, userAuthentication } from "../controllers/user-controllers.js"
 
 const router = express.Router()
 
 router.post('/register',register)
+router.post('/login',userAuthentication)
 
 export default router
